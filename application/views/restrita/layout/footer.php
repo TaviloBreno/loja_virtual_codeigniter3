558c29e56e8cf2219ad2249d6a34295ea78df60e
Tavilo Breno <breno_wk2@hotmail.com>
2025-12-11 16:08:35 -0300

    Add Ion Auth configuration and layout files for the admin dashboard
    - Created ion_auth.php configuration file for user authentication settings.
    - Added footer.php layout file for the main footer of the admin dashboard.
    - Implemented header.php layout file for the main header of the admin dashboard.
    - Developed navbar.php layout file for the navigation bar of the admin dashboard.
    - Created sidebar.php layout file for the sidebar navigation of the admin dashboard.

---
+++ application/views/restrita/layout/footer.php
@@ -0,0 +1,25 @@
+<footer class="main-footer">
+				<div class="footer-left">
+					<a href="#">Agência B-Web</a>
+				</div>
+				<div class="footer-right">
+				</div>
+			</footer>
+		</div>
+	</div>
+	<!-- General JS Scripts -->
+	<script src="<?php echo base_url('public/'); ?>assets/js/app.min.js"></script>
+	<!-- JS Libraies -->
+	<script src="<?php echo base_url('public/'); ?>assets/bundles/apexcharts/apexcharts.min.js"></script>
+	<!-- Page Specific JS File -->
+	<script src="<?php echo base_url('public/'); ?>assets/js/page/index.js"></script>
+	<!-- Template JS File -->
+	<script src="<?php echo base_url('public/'); ?>assets/js/scripts.js"></script>
+	<!-- Custom JS File -->
+	<script src="<?php echo base_url('public/'); ?>assets/js/custom.js"></script>
+</body>
+
+
+<!-- index.html  21 Nov 2019 03:47:04 GMT -->
+
+</html>
