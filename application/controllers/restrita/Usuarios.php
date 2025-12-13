eeb20d041cc09b50b8fe825e6c9396a985a47f7d
Tavilo Breno <breno_wk2@hotmail.com>
2025-12-13 15:40:45 -0300

    Update configuration and language files: remove index page from config, enable CSRF protection, and add Portuguese-Brazilian translations for authentication and calendar.

--- application/controllers/restrita/Usuarios.php
+++ application/controllers/restrita/Usuarios.php
@@ -1,15 +1,18 @@
 <?php
 
-defined('BASEPATH') OR exit('Ação não permitida');
+defined('BASEPATH') or exit('Ação não permitida');
 
 
-class Usuarios extends CI_Controller {
+class Usuarios extends CI_Controller
+{
 
-	public function __construct() {
+	public function __construct()
+	{
 		parent::__construct();
 	}
 
-	public function index() {
+	public function index()
+	{
 		$data = array(
 			'usuarios' => $this->ion_auth->users()->result(),
 			'titulo' => 'Usuários do sistema',
@@ -30,26 +33,52 @@ class Usuarios extends CI_Controller {
 		$this->load->view('restrita/layout/footer');
 	}
 
-	public function core($usuario_id = NULL) {
-		if(!$usuario_id){
+	public function core($usuario_id = NULL)
+	{
+		if (!$usuario_id) {
+		} else {
+			if (!$usuario = $this->ion_auth->user($usuario_id)->row()) {
 
-		}else{
-			if(!$usuario = $this->ion_auth->user($usuario_id)->row()){
-				
 				$this->session->set_flashdata('error', 'Usuário não encontrado');
 				redirect('restrita/usuarios');
+			} else {
 
-			}else{
-				$data = array(
-					'titulo' => 'Editar usuário',
-					'usuario' => $this->ion_auth->user($usuario_id)->row(),
-					'perfil' => $this->ion_auth->get_users_groups($usuario_id)->row(),
-					'grupos' => $this->ion_auth->groups()->result(),
-				);
+				$this->form_validation->set_rules('first_name', 'Nome', 'trim|required|min_length[4]|max_length[45]');
+				$this->form_validation->set_rules('last_name', 'Sobrenome', 'trim|required|min_length[4]|max_length[45]');
+				$this->form_validation->set_rules('email', 'E-mail', 'trim|required|valid_email|min_length[4]|max_length[100]');
+				$this->form_validation->set_rules('username', 'Usuário', 'trim|required|min_length[4]|max_length[45]');
+				$this->form_validation->set_rules('active', 'Ativo', 'trim|required|in_list[0,1]');
 
-				$this->load->view('restrita/layout/header', $data);
-				$this->load->view('restrita/usuarios/core');
-				$this->load->view('restrita/layout/footer');
+				if ($this->form_validation->run()) {
+					$data = elements(
+						array(
+							'first_name',
+							'last_name',
+							'email',
+							'username',
+							'active',
+						),
+						$this->input->post()
+					);
+
+					$data = html_escape($data);
+
+					$this->ion_auth->update($usuario_id, $data);
+
+					redirect('restrita/usuarios');
+				} else {
+
+					$data = array(
+						'titulo' => 'Editar usuário',
+						'usuario' => $this->ion_auth->user($usuario_id)->row(),
+						'perfil' => $this->ion_auth->get_users_groups($usuario_id)->row(),
+						'grupos' => $this->ion_auth->groups()->result(),
+					);
+
+					$this->load->view('restrita/layout/header', $data);
+					$this->load->view('restrita/usuarios/core');
+					$this->load->view('restrita/layout/footer');
+				}
 			}
 		}
 	}
